98f299c3a4239cbaf9ddfc2571cb3c502515b195
Matthew Bresette <bresettem@gmail.com>
2018-10-09 21:55:49 -0500

    Added insert into database from csv file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,60 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+def insert_tlds
+  Tld.delete_all
+  csv_text = File.read("#{File.expand_path('../..', __FILE__)}/lib/seeds/tlds_db.csv")
+  csv      = CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1')
+  csv.each_with_index do |row, index|
+    t      = Tld.new
+    t.name = row['name']
+    t.typ  = row['typ']
+    t.save
+    puts "#{index}: #{t.name}, #{t.typ} saved"
+  end
+  puts "There are now #{Tld.count} rows in the tlds table"
+end
+
+def insert_words
+  Word.delete_all
+  csv_text = File.read("#{File.expand_path('../..', __FILE__)}/lib/seeds/words_db.csv")
+  csv      = CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1')
+  csv.each_with_index do |row, index|
+    t        = Word.new
+    t.typ    = row['typ']
+    t.spacer = row['spacer']
+    t.word   = row['word']
+    t.save
+    puts "#{index}: #{t.typ}, #{t.spacer}, #{t.word} saved"
+  end
+  puts "There are now #{Word.count} rows in the words table"
+end
+
+def insert_domains
+  Domain.delete_all
+  csv_text = File.read("#{File.expand_path('../..', __FILE__)}/lib/seeds/domains_db.csv")
+  csv      = CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1')
+  csv.each_with_index do |row, index|
+    t            = Domain.new
+    t.available  = row['available']
+    t.currency   = row['currency']
+    t.definitive = row['definitive']
+    t.domain     = row['domain']
+    t.period     = row['period']
+    t.price      = row['price']
+    t.score      = row['score']
+    t.spacer     = row['spacer']
+    t.tld        = row['tld']
+    t.typ        = row['typ']
+    t.save
+    puts "#{index}: #{t.available}, #{t.currency}, #{t.definitive}, #{t.domain}, #{t.period}, #{t.price}, #{t.score}, #{t.spacer}, #{t.tld}, #{t.spacer} saved"
+  end
+  puts "There are now #{Domain.count} rows in the domains table"
+end
+
+insert_tlds
+insert_words
+insert_domains
